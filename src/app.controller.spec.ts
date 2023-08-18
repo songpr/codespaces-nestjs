71c8ab7451c15229e56d70061769dec84bf80c42
Pornchai.Rungsirijaratthong <Pornchai.Rungsirijaratthong@wundermanthompson.com>
2023-08-18 11:09:14 +0700

    support hello name

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,9 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+
+    it('should return "Hello Song"', () => {
+      expect(appController.getHello('Song')).toBe('Hello Song');
+    });
   });
 });
